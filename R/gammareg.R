b4cc65fa578832a201ec6d90e03c44713eef44c6
abikoushi <a.b.e.k.o.69@gmail.com>
2020-02-13 23:34:18 +0900

    kakikake

--- R/gammareg.R
+++ R/gammareg.R
@@ -7,11 +7,13 @@ llgamma <- function(par,y,d,X){
   a <- exp(par[1])
   beta <- par[-1]
   b <- exp(X%*%beta)
-  -sum(d*dgamma(y,a,b,log=TRUE)+
-    (1-d)*pgamma(y,a,b,log.p =TRUE,lower.tail = FALSE))
+  sum(d*dgamma(y,a,scale=b,log=TRUE)+
+    (1-d)*pgamma(y,a,scale=b,log.p =TRUE,lower.tail = FALSE))
 }
-X
-opt <- optim(numeric(3),llgamma,y=gehan$time,d=gehan$cens,X=X,control = list(maxit=5000))
+
+opt <- optim(numeric(3),llgamma,y=gehan$time,d=gehan$cens,X=X,
+             control = list(fnscale=-1,maxit=5000))
 
 logLik(fitwei)
+opt$value
 exp(opt$par[1])
